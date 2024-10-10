ba9ff4db9fa07e49cb397488429de152006de4dc
Jeppe Christiansen <jeppe@codr.dk>
2024-10-10 13:11:39 +0200

    chore: formatting

--- examples/example.ts
+++ examples/example.ts
@@ -2,15 +2,15 @@ import { Ok, Err, type Result } from '../src';
 
 // The generic `Result`-type must be set on the function signature
 function greetings(name?: string): Result<string, Error> {
-	if (name === 'jeppech') {
-		return Err(new Error('I will not greet jeppech!'));
-	}
+  if (name === 'jeppech') {
+    return Err(new Error('I will not greet jeppech!'));
+  }
 
-	if (name === undefined) {
-		return Ok('Guest');
-	}
+  if (name === undefined) {
+    return Ok('Guest');
+  }
 
-	return Ok(name);
+  return Ok(name);
 }
 
 // Try enabling inlayHints for JS/TS on the Language Server, to see the inferred types.
@@ -26,17 +26,17 @@ const t4 = greetings('user').ok();
 
 // `is_ok` and `is_err` can be used to narrow the type
 if (t1.is_ok()) {
-	// OkResult<string, Error>
-	const t4 = t1;
+  // OkResult<string, Error>
+  const t4 = t1;
 } else {
-	// ErrResult<string, Error>
-	const t4 = t1;
+  // ErrResult<string, Error>
+  const t4 = t1;
 }
 
 if (t1.is_err()) {
-	const t4 = t1;
+  const t4 = t1;
 } else {
-	const t4 = t1;
+  const t4 = t1;
 }
 
 // Option<string>
@@ -44,14 +44,14 @@ const o1 = t4;
 
 // `is_some` and `is_none` can be used to narrow the type
 if (o1.is_none()) {
-	// NoneOption<string>
-	const o2 = o1;
+  // NoneOption<string>
+  const o2 = o1;
 } else if (o1.is_some()) {
-	// SomeOption<string>
-	const o2 = o1;
+  // SomeOption<string>
+  const o2 = o1;
 } else {
-	// never
-	const o2 = o1;
+  // never
+  const o2 = o1;
 }
 
 // string
@@ -63,4 +63,4 @@ const e2 = o1.expect('this does not throw, because the option is not None.');
 //         ^- this is a Option<string>
 
 // Result<void, never>
-const void_result = Ok()
+const void_result = Ok();
